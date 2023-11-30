757661a2998d5c5733dad80c92d867486bdc8173
PriAurin <146650226+PriAurin@users.noreply.github.com>
2023-11-30 17:26:41 +0600

    Add files via upload

---
+++ JavaApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package aurin.com.javaapplication;
+
+/**
+ *
+ * @author SK
+ */
+public class JavaApplication {
+
+    public static void main(String[] args) {
+        double x = Math.random();
+        System.out.println(x);
+    }
+}
